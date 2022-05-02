a3f0822b797c2cb8a57e17b1e1f54cd1a0c769b2
coding-sta <personal.sta.bd@gmail.com>
2022-05-03 05:44:39 +0600

    single data receive

--- index.js
+++ index.js
@@ -1,19 +1,51 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const app=express();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
 //port
-const port=process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 //MIDDLE WARE
 app.use(cors());
 app.use(express.json());
 
-//api
-app.get('/',(req,res)=>{
-    res.send('server is running')
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htqi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+      await client.connect()
+      const carCollection=client.db('marcedex-warehouse').collection('cars')
+app.get('/car',async (req,res)=>{
+    const query={};
+    const cursor=carCollection.find(query);
+    const cars=await cursor.toArray();
+    res.send(cars)
+
+
 })
 
-app.listen(port,()=>{
-    console.log('listening to port');
+app.get('/car/:id',async(req,res)=>{
+    const id=req.params.id;
+    const query= {_id:ObjectId(id)}
+    const car=await carCollection.findOne(query);
+    res.send(car);
 })
+  } 
+  finally {
+  }
+}
+run().catch(console.dir);
+
+//api
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
+
+app.listen(port, () => {
+  console.log("listening to port");
+});
