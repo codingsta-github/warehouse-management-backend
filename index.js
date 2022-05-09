0b053afcb106e0a2a136e9ba8006d670f3c67256
coding-sta <personal.sta.bd@gmail.com>
2022-05-09 16:56:29 +0600

    jwt issue

--- index.js
+++ index.js
@@ -11,6 +11,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send("forbidden access");
+    }
+    req.decoded = decoded;
+  });
+  next();
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.htqi5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -37,13 +52,18 @@ async function run() {
       res.send(cars);
     });
 
-    app.get("/userCar", async (req, res) => {
+    app.get("/userCar", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
+
       const email = req.query.email;
-      const query = { email: email };
-      console.log(email);
-      const cursor = carCollection.find(query);
-      const cars = await cursor.toArray();
-      res.send(cars);
+      if (decodedEmail === email) {
+        const query = { email: email };
+        const cursor = carCollection.find(query);
+        const cars = await cursor.toArray();
+        res.send(cars);
+      }
+      else{res.status(403).send({message:'forbidden access'})}
+      
     });
 
     app.get("/car/:id", async (req, res) => {
@@ -98,5 +118,5 @@ app.get("/", (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("listening to portfdf dddddfdfdf");
+  console.log("listening to ");
 });
