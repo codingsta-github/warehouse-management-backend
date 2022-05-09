7b0bb66bb3ed0f48cad43106710fbb041e0af5cb
coding-sta <personal.sta.bd@gmail.com>
2022-05-09 18:04:53 +0600

    jwt issue

--- index.js
+++ index.js
@@ -16,6 +16,13 @@ function verifyJWT(req, res, next) {
   if (!authHeader) {
     return res.status(401).send({ message: "unauthorized access" });
   }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(403).send("forbidden access");
+    }
+    req.decoded = decoded;
+  });
   next();
 }
 
@@ -46,11 +53,16 @@ async function run() {
     });
 
     app.get("/userCar", verifyJWT, async (req, res) => {
+      const decodedEmail = req.decoded.email;
       const email = req.query.email;
-      const query = { email: email };
-      const cursor = carCollection.find(query);
-      const cars = await cursor.toArray();
-      res.send(cars);
+      if (decodedEmail === email) {
+        const query = { email: email };
+        const cursor = carCollection.find(query);
+        const cars = await cursor.toArray();
+        res.send(cars);
+      }
+      else{res.status(403).send({message:'forbidden access'})}
+      
     });
 
     app.get("/car/:id", async (req, res) => {
@@ -92,7 +104,7 @@ async function run() {
       const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1d",
       });
-      res.send({ token });
+      res.send({ accessToken });
     });
   } finally {
   }
